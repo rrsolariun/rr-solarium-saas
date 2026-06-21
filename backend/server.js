ac19ea08cbe588dbf97f431cd811cabd4f47d12d
rrsolariun <rrsolarium2026@gmail.com>
2026-06-21 19:24:41 +0000

    Backend funcionando

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    sistema: "RR Solarium",
+    status: "online"
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
